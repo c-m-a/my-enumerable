c81d44378cf5cbeae13e803ff3d45c3b16c65188
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2019-12-13 13:32:52 -0500

    Add my_each_with_index method

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -11,5 +11,21 @@ module Enumerable
       yield(array[i])
       i += 1
     end
+
+    self
+  end
+
+  def my_each_with_index
+    return p to_enum(__method__) unless block_given?
+
+    i = 0
+    array = self.class == Array ? self : to_a
+
+    while i < size
+      yield(array[i], i)
+      i += 1
+    end
+
+    self
   end
 end
