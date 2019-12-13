695fe591f2c75519a1287d2f80dc895978ea7111
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2019-12-13 13:06:42 -0500

    Add my enumerable module and create #my_each block method

---
+++ lib/enumerable.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Enumerable
+  def my_each
+    return p to_enum(__method__) unless block_given?
+
+    i = 0
+    array = self.class == Array ? self : to_a
+
+    while i < size
+      yield(array[i])
+      i += 1
+    end
+  end
+end
