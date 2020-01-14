c8c5f6cfa913cc665071941ea9116fb493b8679e
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2020-01-14 16:24:40 -0500

    Add my_all context given and no given block test case

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -52,3 +52,20 @@ RSpec.describe Enumerable, '#my_select' do
     end
   end
 end
+
+RSpec.describe Enumerable, '#my_all?' do
+  context 'no block given' do
+    let(:arr) { [1, 2] }
+    it 'returns Boolean' do
+      native = arr.all?.class
+      expect(arr.my_all?.class).to eq native
+    end
+  end
+  context 'block given' do
+    let(:arr) { [1, 2] }
+    it 'returns Enumerator' do
+      native = arr.all? { |n| arr.include?(n) }
+      expect(arr.my_all? { |n| arr.include?(n) }).to eq native
+    end
+  end
+end
