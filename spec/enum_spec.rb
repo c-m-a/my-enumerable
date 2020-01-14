f160e9ae5bf6ddc7500f31d1389abaeb1b2bbe42
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2020-01-14 15:55:45 -0500

    Fix it comment in my_each testing case and adding a my_each_with_index testing case

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -5,16 +5,26 @@ require 'enumerable'
 RSpec.describe Enumerable, '#my_each' do
   context 'no block given' do
     let(:arr) { [1, 2] }
-    it 'no block given return Enumerator' do
+    it 'returns Enumerator' do
       native = arr.each.class
       expect(arr.my_each.class).to eq native
     end
   end
   context 'block given' do
     let(:arr) { [1, 2] }
-    it 'return Enumerator' do
+    it 'returns Enumerator' do
       native = arr.each { |n| arr.include?(n) }
       expect(arr.my_each { |n| arr.include?(n) }).to eq native
+    end
+  end
+end
+
+RSpec.describe Enumerable, '#my_each_with_index' do
+  context 'no block given' do
+    let(:arr) { [1, 2] }
+    it 'returns Enumerator' do
+      native = arr.each_with_index.class
+      expect(arr.my_each_with_index.class).to eq native
     end
   end
 end
