79933c2a2c2bc3b2b1d6b514f961709c28af0ed8
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2020-01-14 16:09:41 -0500

    Add my_select testing case block and no block given context

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -35,3 +35,20 @@ RSpec.describe Enumerable, '#my_each_with_index' do
     end
   end
 end
+
+RSpec.describe Enumerable, '#my_select' do
+  context 'no block given' do
+    let(:arr) { [1, 2] }
+    it 'returns Enumerator' do
+      native = arr.select.class
+      expect(arr.my_each_with_index.class).to eq native
+    end
+  end
+  context 'block given' do
+    let(:arr) { [1, 2] }
+    it 'returns Enumerator' do
+      native = arr.select.class
+      expect(arr.my_select.class).to eq native
+    end
+  end
+end
